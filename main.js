1bac9ee72700cf9f5578312bf836678cf933e0f8
Marino Vasaro <marinovasaro.3@gmail.com>
2022-09-01 14:02:48 -0300

    push para guardar, nada finalizado

--- main.js
+++ main.js
@@ -1,33 +1,63 @@
+// function descuento() {
+//     let descuento = prompt('Tiene codigo de descuento?')
+//         if (descuento == 'si'|| (descuento) == 'SI' || (descuento) =='Si'){
+//             let ingreseCodigo = parseInt(prompt('Ingrese el codigo'))
+//             if (ingreseCodigo = descuento){
+//                 alert('CODIGO ACEPTADO CORRECTAMENTE, 15% DESCUENTO')
+//                 porcentaje = precio / 0.15;
+//                 precio = (precio - porcentaje);
+//             }
+//         else{
+//             alert('codigo incorrecto')
+//         }
+//     }
+//         else if (descuento == 'no'|| (descuento) == 'NO' || (descuento) =='No'){
+//             alert('Opcion Incorrecta.')
+//         } 
+//     }
 
-let edad = parseInt(prompt("Ingresa tu edad"));
-if(edad < 15){
-    alert("No puedes entrar")
-    location.href ="http://www.google.com";
-    
-}
-if(edad >= 18){
-    alert("Puedes entrar a comprar")
+//DECLARACION DE VARIABLES VACIAS
 
-function total(){
-    let producto = prompt("Ingrese el valor del producto");
-        producto = producto * 1.21;
-    let descuento = prompt('ingrese su descuento');
-    let precioFinal = (producto - descuento);   
-    alert("El precio final de su preducto, sumado el iva y realizado el descuento es de "+ "$" + precioFinal);
-}
-}
+let eleccion;
+let precio
+let producto;
+const descuento = 544312;
 
-total();
-
-
-//Esto es para ejecutar en el VS con un ejemplo de un ciclo.
-
-for(let i = 0; i < 100; i = i+2){
-    if(i === 50){
-        continue;
+//CONSTRUCTOR
+class usuario{
+    constructor(producto, precio){
+        this.producto = producto,
+        this.precio = precio
     }
-    console.log('Numeros pares del 0 al 100 menos el cincuenta', i);
 }
 
+for (let i = 0; i < 2; i++){
+    let eleccion = prompt('Ingrese el perfume deseado \n 1- Paco Rabanne-Invictus \n 2- Paco Rabanne-One Millon \n 3- Carolina Herrera-VIP');
+    if (eleccion == 1){
+        precio = '$' + 12500;
+        producto = 'Paco Rabanne-Invictus';
+        descuento()
+    }
+    else if (eleccion == 2){
+        precio = '$' + 11350;
+        producto = 'Paco Rabanne-Invictus';
+        descuento()
+    }
+    else if (eleccion == 3){
+        precio = '$' + 14200;
+        producto = 'Paco Rabanne-Invictus';
+        descuento()
+    }
+    else{
+        alert('Su eleccion es incorrecta o no se encuentra en stock, porfavor coloque el numero de las opciones.')
+    }
+    //Push al array (datos)
+        var compra1 = new usuario (producto, precio);
+        datos.push(compra1) 
+        alert(`Su ${compra1.producto} cuesta ${precio}`)
+}
+
+//ARRAY PARA GUARDAR LOS DATOS DEL USUARIO CON UN PUSH
+var datos = []
 
 
